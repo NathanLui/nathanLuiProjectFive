d4c32f662c6c8421d9f87a937254d1d998dde891
Nathan <nathanlui429@gmail.com>
2019-02-28 20:32:25 -0500

    first button does a thing

--- src/App.js
+++ src/App.js
@@ -7,44 +7,63 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      wordList: []
+      wordList: [],
+      seen: false
     }
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Verbal Memory Game!</h1>
-        </header>
-        <section>
-        {this.state.wordList.map((word) => {
-            return ( 
-              <p>{word[Math.floor(Math.random() * word.length)]}</p>
-              )
-          })}
-          <button>Seen</button>
-          <button>New</button>
-        </section>
-      </div>
-    );
-  }
-
-  componentDidMount() {
+  handleClickSeen = e => {
+    e.preventDefault();
     const dbRef = firebase.database().ref();
     dbRef.on('value', (response) => {
       const newState = [];
       const data = response.val();
-
-      for (let key in data) {
-        newState.push(data[key]);
+      for (let randomWord in data) {
+        newState.push(data[randomWord]);
       }
-
       this.setState({
-        wordList: newState
+        wordList: newState,
       });
-    });
+      return (
+        <p>{newState[Math.floor(Math.random() * newState.length)]}</p>
+      )
+    })
   }
-}  
+    
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Verbal Memory Game!</h1>
+          </header>
+          <section> 
+          {this.state.wordList.map((word) => {
+              return ( 
+                <p>{word[Math.floor(Math.random() * word.length)]}</p>
+                )
+            })}
+            <button onClick={this.handleClickSeen}>Seen</button>
+            <button>New</button>
+          </section>
+        </div>
+      );
+    }
+
+    componentDidMount() {
+      const dbRef = firebase.database().ref();
+      dbRef.on('value', (response) => {
+        const newState = [];
+        const data = response.val();
   
+        for (let key in data) {
+          newState.push(data[key]);
+        }
+        this.setState({
+          wordList: newState,
+        });
+      });
+    }
+  }
+
+
 export default App;
