5e33ea214c6b25e37e3a0ebdc0bab0f0204c20f9
Nathan <nathanlui429@gmail.com>
2019-03-04 06:39:15 -0500

    FML

--- src/App.js
+++ src/App.js
@@ -2,68 +2,171 @@ import React, { Component } from 'react';
 import './App.css';
 import firebase from './firebase.js';
 
+import Title from './Title.js'
+import Game from './Game.js'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      wordList: [],
-      seen: false
+      wordList: [
+        {
+          word: 'React',
+          seen: false
+        },
+        {
+          word: 'Firebase',
+          seen: false
+        },
+        {
+          word: 'Hacker',
+          seen: false
+        },
+        {
+          word: 'Element',
+          seen: false
+        },
+        {
+          word: 'Semantic',
+          seen: false
+        },
+        {
+          word: 'Javascript',
+          seen: false
+        },
+        // {
+        //   word: 'Developer',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Conditional',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Property',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Function',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Document',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Responsive',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Wirefame',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Front-end',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Back-end',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Domain',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Designer',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Typography',
+        //   seen: false
+        // },
+        // {
+        //   word: 'Memory',
+        //   seen: false
+        // }
+      ],
+      displayedWord: ['Test Your Memory!'],
+      score: 0,
+      lives: 3,
+      previousScores: []
     }
+  }
+
+  handleClickNew = e => { 
+    e.preventDefault();
+    const randoWord = Math.floor(Math.random() * this.state.wordList.length);
+    const newState = [];
+    newState.push(this.state.wordList[randoWord].word, this.state.wordList[randoWord].seen)
+    this.setState({
+      displayedWord: newState,
+    });
+    if (this.state.wordList[randoWord].seen === false) {
+      this.state.wordList[randoWord].seen = true
+      this.setState({
+        score: this.state.score +1,
+      })
+    }
+    else if (this.state.wordList[randoWord].seen === true) {
+      this.setState({
+         lives: this.state.lives -1
+      }) 
+    }
+      if (this.state.lives < 2) {
+        alert('Gameover!')
+        window.location.reload();  
+      }
   }
 
   handleClickSeen = e => {
     e.preventDefault();
-    const dbRef = firebase.database().ref();
-    dbRef.on('value', (response) => {
-      const newState = [];
-      const data = response.val();
-      for (let randomWord in data) {
-        newState.push(data[randomWord]);
-      }
+    const randoWord = Math.floor(Math.random() * this.state.wordList.length);
+    const newState = [];
+    newState.push(this.state.wordList[randoWord].word, this.state.wordList[randoWord].seen) 
+    this.setState({
+      displayedWord: newState
+    });
+    if (this.state.wordList[randoWord].seen === false) {
+      this.state.wordList[randoWord].seen = true
+    }
+    else if (this.state.wordList[randoWord].seen === true) {
       this.setState({
-        wordList: newState,
-      });
-      return (
-        <p>{newState[Math.floor(Math.random() * newState.length)]}</p>
-      )
+        score: this.state.score + 1
+      })
+    }
+    else this.setState({
+      lives: this.state.lives - 1
     })
+      if (this.state.lives < 2) {
+        alert('Gameover!')
+        window.location.reload();
+      }
+   
   }
-    
-    render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <h1>Verbal Memory Game!</h1>
-          </header>
-          <section> 
-          {this.state.wordList.map((word) => {
-              return ( 
-                <p>{word[Math.floor(Math.random() * word.length)]}</p>
-                )
-            })}
-            <button onClick={this.handleClickSeen}>Seen</button>
-            <button>New</button>
-          </section>
-        </div>
-      );
-    }
-
-    componentDidMount() {
-      const dbRef = firebase.database().ref();
-      dbRef.on('value', (response) => {
-        const newState = [];
-        const data = response.val();
   
-        for (let key in data) {
-          newState.push(data[key]);
-        }
-        this.setState({
-          wordList: newState,
-        });
-      });
-    }
-  }
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Game />
+          <Title />
+        </header>
+          <h3 className="score">Score: {this.state.score}</h3>
+          <h3 className="lives">Lives: {this.state.lives}</h3>
+          <p className="displayedWord">{this.state.displayedWord}</p>
+          <button onClick={this.handleClickSeen}>Seen</button>
+          <button onClick={this.handleClickNew}>New</button>
+      </div>
+    );
+  }
+}
 
 export default App;
+    
+
+
+
+
+
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Title extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1 className="title">Random Asynchronous Memory Game</h1>
+            </div>
+        )
+    }
+}
